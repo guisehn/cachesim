116e7a18fdb324d89230e0741b4c51b4cf5c2ced
Guilherme Sehn <ghsehn@gmail.com>
2015-06-16 19:08:26 -0300

    Política de substituição LRU implementada.

--- src/br/unisc/computador/politicas/PoliticaSubstituicaoLRU.java
+++ src/br/unisc/computador/politicas/PoliticaSubstituicaoLRU.java
@@ -1,16 +1,95 @@
 package br.unisc.computador.politicas;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class PoliticaSubstituicaoLRU implements PoliticaSubstituicao {
+    
+    private final Map<Object, InfoBloco> infos;
+    private final AtomicInteger timestamp;
+    
+    public PoliticaSubstituicaoLRU() {
+        infos = new HashMap<>();
+        timestamp = new AtomicInteger(0);
+    }
     
     @Override
     public int calcularPosicaoSubstituicao(Object[] conjunto) {
-        return 0;
+        InfoBloco[] infosConjunto = Arrays.stream(conjunto).map(bloco -> getInfoBloco(bloco)).toArray(s -> new InfoBloco[s]);
+        Object blocoMaisAntigo = buscarBlocoMaisAntigo(infosConjunto).getBloco();
+        int indiceBlocoMaisAntigo = 0;
+        
+        for (int i = 0; i < conjunto.length; i++) {
+            if (conjunto[i] == blocoMaisAntigo) {
+                indiceBlocoMaisAntigo = i;
+                break;
+            }
+        }
+
+        return indiceBlocoMaisAntigo;
     }
 
     @Override
-    public void marcarBlocoGravado(Object bloco) { }
+    public void marcarBlocoLido(Object bloco) {
+        InfoBloco info = getInfoBloco(bloco);
+        info.setTimestampUltimaVisita(timestamp.incrementAndGet());
+    }
     
     @Override
-    public void marcarBlocoLido(Object bloco) { }
+    public void marcarBlocoGravado(Object bloco) {
+        InfoBloco info = getInfoBloco(bloco);
+        info.setTimestampUltimaVisita(timestamp.incrementAndGet());
+    }
+    
+    private InfoBloco buscarBlocoMaisAntigo(InfoBloco[] infos) {
+        InfoBloco maisAntigo = null;
+        
+        for (InfoBloco info : infos) {
+            if (maisAntigo == null || info.getTimestampUltimaVisita() < maisAntigo.getTimestampUltimaVisita()) {
+                maisAntigo = info;
+            }
+        }
+        
+        return maisAntigo;
+    }
+    
+    private InfoBloco getInfoBloco(Object bloco) {
+        InfoBloco info;
+        
+        if (infos.containsKey(bloco)) {
+            info = infos.get(bloco);
+        } else {
+            info = new InfoBloco(bloco);
+            infos.put(bloco, info);
+        }
+
+        return info;
+    }
+    
+    private class InfoBloco {
+        
+        private final Object bloco;
+        private int timestampUltimaVisita;
+        
+        public InfoBloco(Object bloco) {
+            this.bloco = bloco;
+            this.timestampUltimaVisita = 0;
+        }
+        
+        public Object getBloco() {
+            return bloco;
+        }
+
+        public long getTimestampUltimaVisita() {
+            return timestampUltimaVisita;
+        }
+
+        public void setTimestampUltimaVisita(int timestampCriacao) {
+            this.timestampUltimaVisita = timestampCriacao;
+        }
+        
+    }
     
 }
